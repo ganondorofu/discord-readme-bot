dcaa211dd74ea192d6083d5e5dd05bcb1165f884
saitogo555 <sg.saitogo@gmail.com>
2025-05-29 01:46:33 +0900

    fix(config): 環境変数のデフォルト値を設定
    - DISCORD_TOKENとCLIENT_IDにデフォルト値を追加

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 // Load environment variables from .env file
 config();
 
-const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-const CLIENT_ID = process.env.CLIENT_ID;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN || "";
+const CLIENT_ID = process.env.CLIENT_ID || "";
 
 export { DISCORD_TOKEN, CLIENT_ID };
