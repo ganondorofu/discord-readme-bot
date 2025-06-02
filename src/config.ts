f1949dcb5370d44b764defee98613e64a91a5910
saitogo555 <sg.saitogo@gmail.com>
2025-06-03 00:48:44 +0900

    feat(config): カラー定数を追加
    - INFO_COLOR, SUCCESS_COLOR, WARNING_COLOR, ERROR_COLORを定義

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import type { ColorResolvable } from "discord.js";
 import { config } from "dotenv";
 
 // .envファイルを読み込む
@@ -7,3 +8,7 @@ export const DISCORD_TOKEN = process.env.DISCORD_TOKEN || "";
 export const CLIENT_ID = process.env.CLIENT_ID || "";
 export const READ_REACTION_EMOJI = process.env.READ_REACTION_EMOJI || "👀";
 export const COMMAND_NAME = process.env.COMMAND_NAME || "readme";
+export const INFO_COLOR: ColorResolvable = 0x3498db;
+export const SUCCESS_COLOR: ColorResolvable = 0x2ecc71;
+export const WARNING_COLOR: ColorResolvable = 0xf1c40f;
+export const ERROR_COLOR: ColorResolvable = 0xe74c3c;
