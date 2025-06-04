454339982a189f91dccf2777cf5d6ae162cec490
saitogo555 <sg.saitogo@gmail.com>
2025-06-04 22:50:09 +0900

    feat(embed): 成功および情報用のEmbedを構築する関数を追加
    - buildSuccessEmbed関数を追加し、成功メッセージを構築
    - buildInfoEmbed関数を追加し、情報メッセージを構築

--- src/utils/embedUtils.ts
+++ src/utils/embedUtils.ts
@@ -1,6 +1,14 @@
 import { EmbedBuilder } from "discord.js";
-import { ERROR_COLOR } from "../config";
+import { ERROR_COLOR, INFO_COLOR, SUCCESS_COLOR } from "../config";
 
 export const buildErrorEmbed = (text: string) => {
 	return new EmbedBuilder().setTitle("❌ エラー").setDescription(text).setColor(ERROR_COLOR);
+};
+
+export const buildSuccessEmbed = (text: string) => {
+	return new EmbedBuilder().setTitle("✅ 成功").setDescription(text).setColor(SUCCESS_COLOR);
+};
+
+export const buildInfoEmbed = (text: string) => {
+	return new EmbedBuilder().setTitle("ℹ️ 情報").setDescription(text).setColor(INFO_COLOR);
 };
